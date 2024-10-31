2a0ff22ece448663b1e17140cc69b8012b9d0b58
yeseul106 <20191037@sungshin.ac.kr>
2024-10-31 14:42:28 +0900

    [FIX] 리프레시 토큰을 쿠키에서 가져오지 못하는 이슈 해결을 위한 로그 추가

--- side/src/main/java/com/ssafy/side/api/auth/controller/AuthController.java
+++ side/src/main/java/com/ssafy/side/api/auth/controller/AuthController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.security.Principal;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/oauth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController implements AuthApi {
 
     private final AuthService authService;
@@ -101,6 +103,7 @@ public class AuthController implements AuthApi {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Void> logout(HttpServletRequest request) {
         String refreshToken = (String) request.getAttribute("refreshToken");
+        log.info("Refresh token: {}", refreshToken);
         authService.logout(refreshToken);
 
         return ResponseEntity.status(HttpStatus.OK).build();

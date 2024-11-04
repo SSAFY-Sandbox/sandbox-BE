a0160f081ea36d9685078ced8096f468a201cdce
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2024-11-04 14:56:33 +0900

    [FEAT] Redis CRUD 추가

---
+++ side/src/main/java/com/ssafy/side/common/util/RedisUtil.java
@@ -0,0 +1,33 @@
+package com.ssafy.side.common.util;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public String getValue(String key) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(stringRedisTemplate.hasKey(key));
+    }
+
+    public void setValueWithExpiration(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void deleteValue(String key) {
+        stringRedisTemplate.delete(key);
+    }
+}
+
